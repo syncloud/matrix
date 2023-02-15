1a55e3dcf1c34d87ceb52081d5ab1a7207611406
Boris Rybalkin <ribalkin@gmail.com>
2023-02-15 18:02:20 +0000

    try unix socket

--- bin/service.matrix.sh
+++ bin/service.matrix.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-exec $DIR/dendrite-monolith-server --config /var/snap/matrix/current/config/matrix.yaml
+exec $DIR/dendrite-monolith-server --config /var/snap/matrix/current/config/matrix.yaml --unix-socket=/var/snap/matrix/common/web.socket
