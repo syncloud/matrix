1420fb08d913a6f02d6b6999ba54b200bf74e375
Boris Rybalkin <ribalkin@gmail.com>
2023-02-15 22:18:22 +0000

    add element web ui

--- bin/service.matrix.sh
+++ bin/service.matrix.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-exec $DIR/dendrite --config /var/snap/matrix/current/config/matrix.yaml --unix-socket=/var/snap/matrix/common/web.socket
+exec $DIR/dendrite --config /var/snap/matrix/current/config/matrix.yaml --unix-socket=/var/snap/matrix/current/matrix.socket
--- download.sh
+++ download.sh
@@ -6,6 +6,7 @@ cd ${DIR}
 ARCH=$(uname -m)
 DOWNLOAD_URL=https://github.com/syncloud/3rdparty/releases/download/
 VERSION=$1
+ELEMENT_VERSION=1.11.23
 apt update
 apt install -y wget
 
@@ -23,3 +24,6 @@ wget https://github.com/cyberb/dendrite/archive/refs/heads/$VERSION.tar.gz -O ma
 tar xf matrix.tar.gz
 mv dendrite-$VERSION matrix
 
+wget https://github.com/vector-im/element-web/releases/download/v$ELEMENT_VERSION/element-v$ELEMENT_VERSION.tar.gz -O element.tar.gz
+tar xf element.tar.gz
+mv element-v$ELEMENT_VERSION ${BUILD_DIR}/element
