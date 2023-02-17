a86df56d0bb4b0b9adb4dc96ae7893e98977bdc3
Boris Rybalkin <ribalkin@gmail.com>
2023-02-17 17:21:07 +0000

    fix db wait

--- bin/service.matrix.sh
+++ bin/service.matrix.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-$DIR/bin/wait-for-db.sh
+$DIR/wait-for-db.sh
 exec $DIR/dendrite --config /var/snap/matrix/current/config/matrix.yaml --unix-socket=/var/snap/matrix/current/matrix.socket
