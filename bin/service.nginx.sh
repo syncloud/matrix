2d910728d2277f7f34212998f3aa273274a96442
Boris Rybalkin <ribalkin@gmail.com>
2023-02-17 17:24:32 +0000

    fix nginx config

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -3,4 +3,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 /bin/rm -f /var/snap/matrix/common/web.socket
-exec ${DIR}/nginx/sbin/nginx -c /snap/matrix/current/config/nginx.conf -p ${DIR}/nginx -e stderr
+exec ${DIR}/nginx/sbin/nginx -c /var/snap/matrix/current/config/nginx.conf -p ${DIR}/nginx -e stderr
