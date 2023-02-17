30facdb7a2bdaf1af88ea91ee847d107258c14c9
Boris Rybalkin <ribalkin@gmail.com>
2023-02-17 18:02:24 +0000

    fix version check

--- bin/wait-for-db.sh
+++ bin/wait-for-db.sh
@@ -2,7 +2,7 @@
 
 retry=0
 retries=100
-while ! diff /snap/mastodon/current/version /var/snap/mastodon/current/version; do
+while ! diff /snap/matrix/current/version /var/snap/matrix/current/version; do
     if [[ $retry -gt $retries ]]; then
         echo "waiting for db failed after $retry attempts"
         exit 1
