114ef1d64d09b876170b683beefac21c064f7b53
Boris Rybalkin <ribalkin@gmail.com>
2023-02-22 23:27:28 +0000

    imagemagick

--- integration/deps.sh
+++ integration/deps.sh
@@ -2,6 +2,6 @@
 DIR=$( cd "$( dirname "$0" )" && pwd )
 
 apt-get update
-apt-get install -y sshpass openssh-client
+apt-get install -y sshpass openssh-client imagemagick
 pip install -r requirements.txt
 mx=1000;my=1000;head -c "$((3*mx*my))" /dev/urandom | convert -depth 8 -size "${mx}x${my}" RGB:- $DIR/images/image-big.png
