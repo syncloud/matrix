e7f5a4c8b81a2d63c3ae1f620f93e5b09eb9cd86
Boris Rybalkin <ribalkin@gmail.com>
2023-02-27 08:06:11 +0000

    --ignore-foreign-tables

--- bin/service.whatsapp.sh
+++ bin/service.whatsapp.sh
@@ -2,4 +2,5 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 $DIR/wait-for-db.sh
-exec $DIR/whatsapp -c /var/snap/matrix/current/config/whatsapp.yaml
+exec $DIR/whatsapp -c /var/snap/matrix/current/config/whatsapp.yaml --ignore-foreign-tables
+
