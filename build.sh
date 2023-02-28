de05dfe7144bc90143b1c6249a33cde09dfcf68c
Boris Rybalkin <ribalkin@gmail.com>
2023-02-28 19:11:52 +0000

    fix whatsapp build

--- build.sh
+++ build.sh
@@ -11,4 +11,5 @@ CGO_ENABLED=1 go build -trimpath -v -o $BUILD_DIR/bin ./cmd/...
 rm $BUILD_DIR/bin/dendrite-*
 
 cd ${DIR}/build/whatsapp-master
-CGO_ENABLED=0 go build -o $BUILD_DIR/bin/whatsapp
+GO_LDFLAGS="-s -w -linkmode external -extldflags -static -X main.Tag=0 -X main.Commit=0 -X 'main.BuildTime=`date '+%b %_d %Y, %H:%M:%S'`'"
+go build -ldflags "$GO_LDFLAGS" -o $BUILD_DIR/bin/whatsapp
