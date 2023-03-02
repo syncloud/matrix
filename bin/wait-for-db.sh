4fcb4f0ba899f89607af6bbb8b76e6a92f0d74b0
Boris Rybalkin <ribalkin@gmail.com>
2023-03-02 00:41:47 +0000

    back to proper db wait

--- bin/wait-for-db.sh
+++ bin/wait-for-db.sh
@@ -2,12 +2,14 @@
 
 retry=0
 retries=100
-while ! snap run matrix.psql matrix -c "" ; do
+NEXT=/snap/matrix/current/version
+CURRENT=/var/snap/matrix/current/version
+while ! diff $NEXT $CURRENT; do
     if [[ $retry -gt $retries ]]; then
-        echo "waiting for db failed after $retry attempts"
+        echo "waiting for db failed after $retry attempts (current: $(cat $CURRENT), next $(cat $NEXT)"
         exit 1
     fi
     retry=$((retry + 1))
-    echo "waiting for db $retry/$retries"
+    echo "waiting for db $retry/$retries (current: $(cat $CURRENT), next $(cat $NEXT)"
     sleep 2
 done
--- hooks/installer.py
+++ hooks/installer.py
@@ -104,6 +104,7 @@ class Installer:
     def upgrade(self):
         self.db.restore()
         self.prepare_storage()
+        self.update_db_version()
 
 
     def initialize(self):
@@ -112,6 +113,7 @@ class Installer:
         self.db.execute('postgres', DB_USER, "ALTER USER {0} WITH PASSWORD '{1}';".format(DB_USER, DB_PASSWORD))
         self.db.execute('postgres', DB_USER, "CREATE DATABASE matrix OWNER {0} TEMPLATE template0 ENCODING 'UTF8';".format(DB_USER))
         self.db.execute('postgres', DB_USER, "GRANT CREATE ON SCHEMA public TO {0};".format(DB_USER))
+        self.update_db_version()
         with open(self.install_file, 'w') as f:
             f.write('installed\n')
         
