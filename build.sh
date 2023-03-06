3948652a6c9002e9201b5b68e297b92271001045
Boris Rybalkin <ribalkin@gmail.com>
2023-03-06 22:01:09 +0000

    fix dendrite static build

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ BUILD_DIR=${DIR}/build/snap
 mkdir -p $BUILD_DIR/bin
 
 cd ${DIR}/build/matrix
-CGO_ENABLED=1 go build -trimpath -v -o $BUILD_DIR/bin ./cmd/...
-rm $BUILD_DIR/bin/dendrite-*
+go build -ldflags '-s -w -linkmode external -extldflags -static' -trimpath -v -o $BUILD_DIR/bin/dendrite ./cmd/dendrite
 
 cd ${DIR}/build
 cat <<EOT >> go.work
@@ -19,7 +18,6 @@ use (
     ./whatsapp-master
     ./mautrix-go-master
 )
-EOT
-
+EOT              
 GO_LDFLAGS="-s -w -linkmode external -extldflags -static -X main.Tag=0 -X main.Commit=0 -X 'main.BuildTime=`date '+%b %_d %Y, %H:%M:%S'`'"
 go build -tags nocrypto -ldflags "$GO_LDFLAGS" -o $BUILD_DIR/bin/whatsapp ./whatsapp-master
