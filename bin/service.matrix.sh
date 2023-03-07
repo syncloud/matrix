90c09fee3767223b3461247f57dfe5990dae4104
Boris Rybalkin <ribalkin@gmail.com>
2023-03-07 22:25:12 +0000

    fix start

--- bin/service.matrix.sh
+++ bin/service.matrix.sh
@@ -3,4 +3,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 $DIR/wait-for-db.sh matrix
 $DIR/wait-for-configure.sh
-exec $DIR/matrix/bin/matrix.sh --config /var/snap/matrix/current/config/matrix.yaml --unix-socket=/var/snap/matrix/current/matrix.socket
+exec $DIR/../matrix/bin/matrix.sh --config /var/snap/matrix/current/config/matrix.yaml --unix-socket=/var/snap/matrix/current/matrix.socket
