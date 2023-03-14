cf2bf8c6920b64165f85d3e2311a7eca09255a2e
Boris Rybalkin <ribalkin@gmail.com>
2023-03-14 21:50:11 +0000

    retry docker build

--- postgresql/build.sh
+++ postgresql/build.sh
@@ -9,7 +9,9 @@ BUILD_DIR=${DIR}/../build/snap/postgresql
 
 docker ps -a -q --filter ancestor=postgres:syncloud --format="{{.ID}}" | xargs docker stop | xargs docker rm || true
 docker rmi postgres:syncloud || true
-docker build --build-arg MAJOR_VERSION=$MAJOR_VERSION -t postgres:syncloud .
+while ! docker build --build-arg MAJOR_VERSION=$MAJOR_VERSION -t postgres:syncloud . ; do
+  echo "retry docker"
+done
 docker run postgres:syncloud postgres --help
 docker create --name=postgres postgres:syncloud
 mkdir -p ${BUILD_DIR}
