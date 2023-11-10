62eafe8e0e48c8471d798aceae79d6f31c14b828
Boris Rybalkin <ribalkin@gmail.com>
2023-11-10 19:38:26 +0000

    fix

--- sliding-sync/build.sh
+++ sliding-sync/build.sh
@@ -5,6 +5,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BUILD_DIR=${DIR}/../build/snap/matrix
 mkdir -p $BUILD_DIR/bin
 
-cd $DIR/../build/sliding-sinc
+cd $DIR/../build/sliding-sync
 go build -o $BUILD_DIR/bin/sliding-sync ./cmd/syncv3
 ldd $BUILD_DIR/bin/sliding-sync
