10bd6207f184f2141be03a77d836f82aeab064ba
Boris Rybalkin <ribalkin@gmail.com>
2023-11-15 20:29:42 +0000

    fix socket

--- bin/service.sliding-sync.sh
+++ bin/service.sliding-sync.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 $DIR/wait-for-db.sh sync
 $DIR/wait-for-configure.sh
 . /var/snap/matrix/current/config/env
-export SYNCV3_BINDADDR=/var/snap/matrix/current/sliding-sync.sock
+export SYNCV3_BINDADDR=/var/snap/matrix/current/sliding-sync.socket
 export SYNCV3_SERVER=/var/snap/matrix/current/matrix.socket
 export SYNCV3_SECRET=$(cat /var/snap/matrix/current/sync.secret)
 exec $DIR/sliding-sync
