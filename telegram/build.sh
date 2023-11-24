efd575893c8a53aa6ebce76c98fafebd50d9a719
Boris Rybalkin <ribalkin@gmail.com>
2023-11-24 12:57:06 +0000

    cp

--- telegram/build.sh
+++ telegram/build.sh
@@ -13,3 +13,4 @@ sed -i "s/$TELEGRAM_API_HASH/{TELEGRAM_API_HASH}/g" $CONFIG
 BUILD_DIR=${DIR}/../build/snap
 cd ${DIR}/../build/mautrix-python
 cp mautrix/appservice/appservice.py $BUILD_DIR/python/usr/local/lib/python3.8/site-packages/mautrix/appservice
+cp mautrix/api.py $BUILD_DIR/python/usr/local/lib/python3.8/site-packages/mautrix
