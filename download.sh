8fd959d3bfffe5869d4ceb51bcacaf74954cc975
Boris Rybalkin <ribalkin@gmail.com>
2024-08-11 18:29:17 +0100

    upgrade drone pipeline

--- download.sh
+++ download.sh
@@ -4,7 +4,6 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 
 ARCH=$(uname -m)
-DOWNLOAD_URL=https://github.com/syncloud/3rdparty/releases/download/
 GO_ARCH=$1
 ELEMENT_VERSION=1.11.73
 apt update
@@ -12,11 +11,6 @@ apt install -y wget
 
 BUILD_DIR=${DIR}/build/snap
 mkdir -p $BUILD_DIR/bin
-
-cd ${DIR}/build
-wget ${DOWNLOAD_URL}/nginx/nginx-${ARCH}.tar.gz
-tar xf nginx-${ARCH}.tar.gz
-mv nginx ${BUILD_DIR}
 
 cd ${DIR}/build
 wget https://github.com/vector-im/element-web/releases/download/v$ELEMENT_VERSION/element-v$ELEMENT_VERSION.tar.gz -O element.tar.gz
@@ -27,7 +21,7 @@ ln -s /var/snap/matrix/current/config/element.json config.json
 
 cd ${DIR}/build
 #wget https://github.com/cyberb/sliding-sync/archive/refs/heads/master.tar.gz
-wget https://github.com/matrix-org/sliding-sync/archive/refs/heads/master.tar.gz
+wget https://github.com/matrix-org/sliding-sync/archive/refs/heads/main.tar.gz
 tar -xf master.tar.gz
 rm -rf master.tar.gz
 mv sliding-sync-main sliding-sync
