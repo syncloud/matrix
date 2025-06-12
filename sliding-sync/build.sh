792bcc53dd397f61d3954149861f0eace0e0316c
Boris Rybalkin <ribalkin@gmail.com>
2025-06-12 10:20:18 +0100

    chmod

--- sliding-sync/build.sh
+++ sliding-sync/build.sh
@@ -6,3 +6,4 @@ ARCH=$2
 BUILD_DIR=${DIR}/../build/snap
 mkdir -p $BUILD_DIR/bin
 wget https://github.com/matrix-org/sliding-sync/releases/download/v$VERSION/syncv3_linux_$ARCH -O $BUILD_DIR/bin/sliding-sync
+chmod +x $BUILD_DIR/bin/sliding-sync
