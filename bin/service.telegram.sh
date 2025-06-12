60ec21b82582afc49ef4cc7990afd79b892a52f6
Boris Rybalkin <ribalkin@gmail.com>
2025-06-12 11:56:49 +0100

    fix telegram service

--- bin/service.telegram.sh
+++ bin/service.telegram.sh
@@ -3,5 +3,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 $DIR/wait-for-db.sh telegram
 $DIR/wait-for-configure.sh
-exec $DIR/../python/bin/python -m mautrix_telegram -c /var/snap/matrix/current/config/telegram.yaml
+#exec $DIR/../python/bin/python -m mautrix_telegram -c /var/snap/matrix/current/config/telegram.yaml
+exec $DIR/telegram -c /var/snap/matrix/current/config/telegram.yaml --ignore-foreign-tables --ignore-unsupported-server
 
