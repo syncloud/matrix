ac9b028ddb01444b40c899d5f061512ea877b340
Boris Rybalkin <ribalkin@gmail.com>
2025-06-12 12:39:57 +0100

    fix

--- telegram/build.sh
+++ telegram/build.sh
@@ -14,7 +14,7 @@ sed -i "s/{TELEGRAM_API_ID}/$TELEGRAM_API_ID/g" $CONFIG
 sed -i "s/{TELEGRAM_API_HASH}/$TELEGRAM_API_HASH/g" $CONFIG
 
 BUILD_DIR=${DIR}/../build/snap
-wget "https://mau.dev/mautrix/telegramgo/-/jobs/artifacts/$VERSION/download?job=build $ARCH v2" -O telegram.zip
+wget "https://mau.dev/mautrix/telegramgo/-/jobs/artifacts/$VERSION/download?job=build%20$ARCH%20v2" -O telegram.zip
 unzip telegram.zip
 mv mautrix-telegram $BUILD_DIR/bin/telegram
 chmod +x $BUILD_DIR/bin/telegram
