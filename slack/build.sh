0be4ff840c2e7ee8e8043e6aadad7e750623ee6f
Boris Rybalkin <ribalkin@gmail.com>
2025-06-13 13:26:15 +0100

    old static

--- slack/build.sh
+++ slack/build.sh
@@ -11,6 +11,5 @@ cd ${DIR}/../build
 wget https://github.com/mautrix/slack/archive/$VERSION.tar.gz
 tar -xf $VERSION.tar.gz
 cd slack-*
-#GO_LDFLAGS="-s -w -linkmode external -extldflags -static -X main.Tag=0 -X main.Commit=0 -X 'main.BuildTime=`date '+%b %_d %Y, %H:%M:%S'`'"
-CGO_ENABLED=0 go build -tags nocrypto -o $BUILD_DIR/bin/slack ./cmd/mautrix-slack
-#go build -tags goolm -ldflags "$GO_LDFLAGS" -o $BUILD_DIR/bin/slack ./cmd/mautrix-slack
+GO_LDFLAGS="-s -w -linkmode external -extldflags -static -X main.Tag=0 -X main.Commit=0 -X 'main.BuildTime=`date '+%b %_d %Y, %H:%M:%S'`'"
+go build -tags nocrypto -ldflags "$GO_LDFLAGS" -o $BUILD_DIR/bin/slack ./cmd/mautrix-slack
